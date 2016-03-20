5d3189bf007e67fd57c64e3903b7824a72811dec
srujangopu <srujanjack@gmail.com>
2016-03-20 18:16:54 -0500

    spring security 3.0

---
+++ src/main/java/com/mkyong/web/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.mkyong.web.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //do nothing
+}
